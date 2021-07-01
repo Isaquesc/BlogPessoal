2cc408d0c86a5e72d833e4de41a8fea9ceadd1c0
isaquec <isaquedeco@hotmail.com>
2021-07-01 10:59:03 -0300

    adicionando o componente tema

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -3,20 +3,18 @@ import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  selector: 'app-tema',
+  templateUrl: './tema.component.html',
+  styleUrls: ['./tema.component.css']
 })
-export class InicioComponent implements OnInit {
+export class TemaComponent implements OnInit {
 
   constructor(
     private rota: Router
   ) { }
 
-  ngOnInit() {
-    window.scroll(0, 0)
-
-    if (environment.token == '') {
+  ngOnInit(){
+     if (environment.token == '') {
       alert('Sua seção expirou, faça o login novamente')
       this.rota.navigate(['/entrar'])
     }
