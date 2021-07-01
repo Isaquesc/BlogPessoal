07e9c66bc20fd910670e6cc4e3e4b960af9b128a
isaquec <isaquedeco@hotmail.com>
2021-07-01 00:57:37 -0300

    componente inicio-sem consulto

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,8 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  foto = environment.foto
+  nome = environment.nome
 
-  ngOnInit(): void {
+  constructor(private rota: Router) { }
+
+  ngOnInit() {
   }
 
+  sair(){
+    this.rota.navigate(['/entrar'])
+    environment.token = ''
+    environment.foto = ''
+    environment.nome = ''
+    environment.id = 0
+  }
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserLogin } from '../model/UserLogin';
 import { User } from '../model/User';
+import { environment } from 'src/environments/environment.prod';
 
 
 @Injectable({
@@ -12,11 +13,19 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  entrar(userLogin: UserLogin): Observable<UserLogin>{
+  entrar(userLogin: UserLogin): Observable<UserLogin> {
     return this.http.post<UserLogin>('https://bpisk.herokuapp.com/usuario/logar', userLogin)
   }
 
-  cadastrar(user: User): Observable<User>{
+  cadastrar(user: User): Observable<User> {
     return this.http.post<User>('https://bpisk.herokuapp.com/usuario/cadastrar', user)
+  }
+
+  logado() {
+    let logado: boolean = false
+    if (environment.token != '') {
+      logado = true
+    }
+    return logado
   }
 }
