8394994cb4b5b2b31ed59b369b9f1d2f630acd18
isaquec <isaquedeco@hotmail.com>
2021-07-05 22:38:49 -0300

    metodos crud

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -23,10 +23,22 @@ export class TemaService {
 
   getAllTema(): Observable<Tema[]> {
     return this.http.get<Tema[]>('https://bpisk.herokuapp.com/tema', this.token)
-  } 
+  }
+
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://bpisk.herokuapp.com/tema/${id}`, this.token)
+  }
 
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://bpisk.herokuapp.com/tema', tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://bpisk.herokuapp.com/tema', tema, this.token)
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(`https://bpisk.herokuapp.com/tema/${id}`, this.token)
   }
 
 }
